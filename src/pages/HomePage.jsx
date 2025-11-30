0b8a1e8c52c938ff6f62ab68f3693425ccb4e76d
valmaiimtiyaz <valmaityz30@gmail.com>
2025-11-30 22:40:04 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import heroImage from "../assets/Rumah Fantasi 1.svg";
-import About from "../components/about";
+import About from "../components/About"; 
 import Gallery from "../components/Gallery";
 import HowItWorks from "../components/HowItWorks";
 import GetStarted from "../components/GetStarted";
@@ -82,3 +82,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
